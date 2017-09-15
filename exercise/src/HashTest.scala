0ea06163b647975ee9a164c935ab90a2175058cd
gergely harmat <gergely.harmat@gmail.com>
2017-09-15 23:03:11 +0200

    commit with beginning of Map example

---
+++ exercise/src/HashTest.scala
@@ -0,0 +1,13 @@
+package hash
+
+import collection.mutable._
+
+object HashTest {
+  def main(args: Array[String]) ={
+    val mymap: Map[String, Int] =Map()
+    mymap += ("1bc" -> 18)
+    mymap += ("def" -> 22)
+    
+
+  }
+}
